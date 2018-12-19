e0006e26db3863f696197beae3c0032c4dfbbc44
adarshrise <adarshrise20@gmail.com>
2018-12-19 14:47:54 +0530

    fibnocii Python code

--- fibnocii/clang/fibnocii.c
+++ fibnocii/clang/fibnocii.c
@@ -1,1 +1,26 @@
-//add your code here
+
+#include<conio.h>
+#include<stdio.h>
+
+void fibo()
+{ long input=0,num1=0,num2=1,i=0; // giving default values
+
+  printf(" Enter the limit for the number of output from the series:- ");
+  scanf("%ld",&input);
+  for(i=0;i<input;i++)
+  {
+    printf(" num%ld : %ld ",i+1,num1+num2); // Printing next number
+
+    num1=num1+num2;       //   moving to the next number by swap
+    num2=num1-num2;
+    num1=num1-num2;
+
+    num2= num1+num2;     //   new value updated
+  }
+}
+
+void main()
+{
+  fibo();       // Fibo Fucntion call
+  getch();
+}
--- fibnocii/python/fibnocii.py
+++ fibnocii/python/fibnocii.py
@@ -1,1 +1,18 @@
-# add your code here
+
+def fibo():
+    num1,num2,into,i =0,1,0,0
+    into=int(input("Enter the limit for the number of output from the series:- "))
+    while(i<into):
+        print(" num ",i+1,": ",num1+num2," ")
+
+        num1=num1+num2;       #   moving to the next number by swap
+        num2=num1-num2;
+        num1=num1-num2;
+        num2=num1+num2;       # next value updated
+        i=i+1
+
+def main():
+    fibo()
+
+if __name__ =='__main__':
+    main()
