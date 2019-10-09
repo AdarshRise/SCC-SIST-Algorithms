60b9adee37b4ce5c94bcd5188df22c93283a24f7
José <jose.alves.neto@ccc.ufcg.edu.br>
2019-10-09 15:23:06 -0300

    queue.py: Add queue class

--- queue/python/queue.py
+++ queue/python/queue.py
@@ -1,1 +1,32 @@
 # add your code here
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+q = Queue()
+
+while True:
+    request = input('''Queue functions:
+            1 E. Enqueue one element; 
+            2. Dequeue one element;
+            3. Finish the program;
+            What do you like to do?''').split()
+    if request[0] == '1':
+        q.enqueue(request[1])
+    elif request[0] == '2':
+        if q.isEmpty():
+            print("Queue is empty")
+        else:
+            print("Dequeued value:", q.dequeue())
+    elif request[0] == '3': 
+        print(q.items)
+        break
